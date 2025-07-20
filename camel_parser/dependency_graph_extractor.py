e87b535c7fcbfe176c6a7109474eb528c0bf1b97
Ahmed Bahloul <ahmed.ehab@jetbrains.com>
2025-07-21 00:21:54 +0200

    remove unnecessary imports

--- camel_parser/dependency_graph_extractor.py
+++ camel_parser/dependency_graph_extractor.py
@@ -3,9 +3,6 @@ import os
 import math
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier  # or XGBoost if you want
-from sklearn.metrics import classification_report
 from datasets import load_dataset
 from datasets import Dataset
 import pyarabic.araby as araby
